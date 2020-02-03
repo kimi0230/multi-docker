b0039676cade0ffc2c8a424427c410fcedcd5a1d
kimi <kimi0230@gmail.com>
2020-02-03 13:33:00 +0800

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATADASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
